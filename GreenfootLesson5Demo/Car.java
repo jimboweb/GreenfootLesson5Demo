2472c50a57b096e38b3197e5190b29d6f2befc58
jimboweb <jimboweb@users.noreply.github.com>
2018-02-23 09:13:14 -0500

    demo lesson

---
+++ GreenfootLesson5Demo/Car.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Car here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Car extends Actor
+{
+    int speed = 5;
+    /**
+     * Act - do whatever the Car wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+       if(Greenfoot.isKeyDown("right")){
+           setRotation(0);
+           move(speed);
+       }
+       if(Greenfoot.isKeyDown("down")){
+           setRotation(90);
+           move(speed);
+       }
+       if(Greenfoot.isKeyDown("left")){
+           setRotation(180);
+           move(speed);
+       }
+       if(Greenfoot.isKeyDown("up")){
+           setRotation(-90);
+           move(speed);
+       }
+       removeTouching(Pizza.class);
+       
+    }    
+    
+}
